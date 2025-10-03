46214b4561ce7b270cf9bc739a6bcd064b976fc9
JaronHel <jaronhellenbrand07@gmail.com>
2025-10-03 11:42:50 +0200

    improved movement, added testmap

--- Scripts/Player/PlayerMovement.gd
+++ Scripts/Player/PlayerMovement.gd
@@ -1,36 +1,28 @@
 extends CharacterBody2D
 
-@export var movementSpeed: float = 400;
+@export var movementSpeed: float = 250;
 @export var movementDeceleration: float = 1000;
 
 @export var rotationSpeed: float = 8;
 var rotationDirection: Vector2 = Vector2.ZERO
 
-@export var dashSpeed: float = 600;
+@export var dashSpeed: float = 350;
 @export var dashDeceleration: float = 1500;
 @export var dashDuration: float = 0.8;
-@export var dashCooldown: float = 2;
 var dashDirection: Vector2 = Vector2.ZERO
 var isDashing: bool = false
-var canDash: bool = true
 var dashTimer: float = 0
-var dashCooldownTimer: float = 1.5
 
 func _physics_process(delta: float) -> void:
-	if dashCooldownTimer > dashCooldown:
-		canDash = true
-	else:
-		dashCooldownTimer += delta
-		
-	if Input.is_action_pressed("move_dash") and canDash:
-		dashCooldownTimer = 0
-		canDash = false
+	if Input.is_action_pressed("move_dash") and !isDashing:
+		rotationDirection = (get_global_mouse_position() - position).normalized()
+		rotation = rotationDirection.angle()
 		isDashing = true
 	
 	if !isDashing:
 		var movementDirection: Vector2 = Vector2(
-			Input.get_action_raw_strength("move_right") - Input.get_action_raw_strength("move_left"),
-			Input.get_action_raw_strength("move_down") - Input.get_action_raw_strength("move_up")
+			Input.get_axis("move_left", "move_right"),
+			Input.get_axis("move_up", "move_down")
 		).normalized()
 		
 		rotationDirection = (get_global_mouse_position() - position).normalized()
